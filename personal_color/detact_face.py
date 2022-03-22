62fceeb1180a2d0919a99db29a781d444f2d8921
yang8331435@naver.com <yang8331435@naver.com>
2022-03-22 22:30:29 +0900

    second commit

--- personal_color/detact_face.py
+++ personal_color/detact_face.py
@@ -1,4 +1,5 @@
 from tkinter import Image
+from turtle import shape
 import dlib 
 import cv2 as cv 
 import numpy as np
@@ -7,16 +8,22 @@ from PIL import Image
 face_det = dlib.get_frontal_face_detector()
 landmark_model = dlib.shape_predictor("/Users/yanghyeok/Desktop/shape_predictor_68_face_landmarks.dat")
 
-ALL = list(range(0, 68))
-RIGHT_EYEBROW = list(range(17, 22))
-LEFT_EYEBROW = list(range(22, 27))
-RIGHT_EYE = list(range(36, 42))
-LEFT_EYE = list(range(42, 48))
-NOSE = list(range(27, 36))
-MOUTH_OUTLINE = list(range(48, 61))
-MOUTH_INNER = list(range(61, 68))
-JAWLINE = list(range(0, 17))
-index = ALL
+right_eyebrow = []
+left_eyebrow = []
+right_eye = []
+left_eye = []
+left_cheek = []
+right_cheek = []
+
+
+face_parts = [[],[],[],[],[],[],[],[]]
+# detect faces in the grayscale image
+rect = detector(cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY), 1)[0]
+# determine the facial landmarks for the face region, then
+# convert the landmark (x, y)-coordinates to a NumPy array
+shape = self.predictor(cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY), rect)
+shape = face_utils.shape_to_np(shape)
+
 
 if __name__ == "__main__": 
     src = cv.imread("/Users/yanghyeok/Desktop/ShowMeTheColor-master/res/test/nfall/1.jpg") 
@@ -34,9 +41,10 @@ if __name__ == "__main__":
         lm_point.append([p.x, p.y]) 
     lm_point = np.array(lm_point) 
     
+    print(lm_point)
+    
     for p in lm_point:
         cv.circle(src, (p[0], p[1]), radius=2, color=(255,0,0), thickness=2) 
     cv.imshow("lsh", src) 
     cv.waitKey() 
     cv.destroyAllWindows()
-    print("R_EYEBROW : ",pix(RIGHT_EYEBROW[0]))
---
+++ personal_color/test.py
@@ -0,0 +1,67 @@
+from cgitb import grey
+from re import S
+from turtle import shape
+from cv2 import waitKey
+import dlib 
+import cv2
+import numpy as np
+from imutils import face_utils
+
+# 얼굴인식
+detector = dlib.get_frontal_face_detector()
+# 얼굴의 68개 landmark 찾기
+predictor = dlib.shape_predictor('/Users/yanghyeok/Desktop/ShowMeTheColor-master/res/shape_predictor_68_face_landmarks.dat')
+#face detection part
+img_path = '/Users/yanghyeok/Desktop/ShowMeTheColor-master/res/test/nfall/1.jpg'
+img = cv2.imread(img_path) #이미지를 컬러로 읽어옴
+# 이미지 읽어오기
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# print(img.shape) (500, 485, 3) -> 3 : rgb
+# print(len(img)) #세로 500
+# print(len(img[0])) #가로 485
+
+# init face parts
+right_eyebrow = []
+left_eyebrow = []
+right_eye = []
+left_eye = []
+left_cheek = []
+right_cheek = []
+
+face_parts = [[],[],[],[],[],[],[],[]]
+# 이미지 그레이 스케일로 변환 -> 컬러영상은 3배많은 메모리를 필요로하기 때문
+grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+rect = detector(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 1)
+# print(rect) -> rectangles[[(98, 27) (419, 348)]]
+
+# cv2.imshow("grey", grey)
+# cv2.waitKey()
+faces = rect
+
+for face in faces: 
+    # 랜드마크 검출 
+    lm = predictor(img, face) 
+    lm_point = [] 
+    for p in lm.parts(): 
+        lm_point.append([p.x, p.y]) 
+    lm_point = np.array(lm_point) 
+        
+    for p in lm_point: 
+        cv2.circle(img, (p[0], p[1]), radius=2, color=(255,0,0), thickness=2)
+
+# cv2.imshow("a",cv2.rectangle(img, (face.left(), face.top()), (face.right(), face.bottom()), color=(0,255,0), thickness=2))
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+rect_1 = detector(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 1)[0]
+shape = predictor(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), rect_1)
+shape = face_utils.shape_to_np(shape)
+
+idx = 0
+for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+            face_parts[idx] = shape[i:j]
+            idx += 1
+face_parts = face_parts[1:5]
+print(face_parts[1])
